26b4798cd51653b87516e794c308a3711fae0295
victor <victor.pc.xd@gmail.com>
2025-12-19 01:52:01 -0500

    19_12_25 1er avance

--- app/Models/Family.php
+++ app/Models/Family.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Family extends Model
