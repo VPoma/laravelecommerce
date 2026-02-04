b23965eb07e3ee44aa3ab77565460692c4c1d945
Victor Poma <victor.pc.xd@gmail.com>
2026-02-04 15:14:33 -0500

    04_02_26 1er avance

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,19 @@
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'route' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Categorías',
+        'route' => route('admin.categories.index'),
+    ],
+    [
+        'name' => 'Nuevo',
+    ],
+]">
+
+<div class="card">
+
+</div>
+
+</x-admin-layout>
