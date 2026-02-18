efbdc40ae79ab36f5995953a75b30b3be17696f9
victor <victor.pc.xd@gmail.com>
2026-02-17 23:43:04 -0500

    17_02_26 2do avance

---
+++ app/Livewire/Admin/Subcategories/SubcategoryEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Subcategories;
+
+use Livewire\Component;
+
+class SubcategoryEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.subcategories.subcategory-edit');
+    }
+}
