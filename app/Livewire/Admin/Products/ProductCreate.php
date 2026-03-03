d0536776d5c02af8f5f0b951188297880ea88ee8
Victor Poma <victor.pc.xd@gmail.com>
2026-03-03 12:35:56 -0500

    03_03_26 2do avance

--- app/Livewire/Admin/Products/ProductCreate.php
+++ app/Livewire/Admin/Products/ProductCreate.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire\Admin\Products;
 
+use App\Models\Category;
 use Livewire\Component;
+use Livewire\Attributes\Computed;
+use App\Models\Family;
+use App\Models\Subcategory;
 
 class ProductCreate extends Component
 {
+
+    public $families;
+    public $family_id = '';
+    public $category_id = '';
+    
+    public $product = [
+        'sku' => '',
+        'name' => '',
+        'description' => '',
+        'image_path' => '',
+        'price' => '',
+        'subcategory_id' => ''
+    ];
+
+    public function mount(){
+        $this->families = Family::all();
+    }
+
+    public function updatedFamilyId($value){
+        $this->category_id = '';
+        $this->product['subcategory_id'] = '';
+    }
+
+    public function updatedCategoryId($value){
+        $this->product['subcategory_id'] = '';
+    }
+
+    #Propiedad computada para traer las categorias de la familia seleccionada
+    #[Computed()]
+    public function categories(){
+        return Category::where('family_id', $this->family_id)->get();
+    }
+
+    #Propiedad computada para traer las subcategorias de la categoria seleccionada
+    #[Computed()]
+    public function subcategories(){
+        return Subcategory::where('category_id', $this->category_id)->get();
+    }
+
     public function render()
     {
         return view('livewire.admin.products.product-create');
--- resources/views/livewire/admin/products/product-create.blade.php
+++ resources/views/livewire/admin/products/product-create.blade.php
@@ -1,4 +1,93 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    -50:39
+<div class="card">
+    
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Código
+        </label>
+
+        <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            wire:model="product.sku"
+            placeholder="Ingrese el código del producto"
+            />
+    </div>
+
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Nombre
+        </label>
+
+        <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            wire:model="product.name"
+            placeholder="Ingrese el nombre del producto"
+            />
+    </div>
+    
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Descripción
+        </label>
+
+        <textarea class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            wire:model="product.description"
+            placeholder="Ingrese la descripción del producto">
+        </textarea>
+    </div>
+
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Familias
+        </label>
+
+        <x-select class="w-full" wire:model.live="family_id">
+
+            <option value="" disabled>
+                Seleccione una familia
+            </option>
+
+            @foreach ($families as $family)
+                <option value="{{ $family->id }}">
+                    {{ $family->name }}
+                </option>
+            @endforeach
+        </x-select>
+    </div>
+
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Categorías
+        </label>
+
+        <x-select class="w-full" wire:model.live="category_id">
+
+            <option value="" disabled>
+                Seleccione una categoría
+            </option>
+
+            @foreach ($this->categories as $category)
+                <option value="{{ $category->id }}">
+                    {{ $category->name }}
+                </option>
+            @endforeach
+        </x-select>
+    </div>
+
+    <div class="mb-4">
+        <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+            Subcategorías
+        </label>
+
+        <x-select class="w-full" wire:model.live="product.subcategory_id">
+
+            <option value="" disabled>
+                Seleccione una subcategoría
+            </option>
+
+            @foreach ($this->subcategories as $subcategory)
+                <option value="{{ $subcategory->id }}">
+                    {{ $subcategory->name }}
+                </option>
+            @endforeach
+        </x-select>
+    </div>
+
 </div>
