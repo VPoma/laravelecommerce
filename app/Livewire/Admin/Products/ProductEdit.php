d5c0d1603372bde816700e710657f461650bc4fe
Victor Poma <victor.pc.xd@gmail.com>
2026-03-06 13:22:47 -0500

    06_03_26 1er avance

--- app/Livewire/Admin/Products/ProductEdit.php
+++ app/Livewire/Admin/Products/ProductEdit.php
@@ -2,11 +2,19 @@
 
 namespace App\Livewire\Admin\Products;
 
+use App\Models\Category;
 use App\Models\Family;
+use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ProductEdit extends Component
 {
+    Use WithFileUploads;
+
     public $product;
 
     public $productEdit;
@@ -15,10 +23,76 @@ class ProductEdit extends Component
     public $family_id = '';
     public $category_id = '';
 
-    public function mount($product)
-    {
+    public $image;
+
+    public function mount($product){
         $this->productEdit = $product->only('sku', 'name', 'description', 'image_path', 'price', 'subcategory_id');
         $this->families = Family::all();
+        $this->category_id = $product->subcategory->category_id;
+        $this->family_id = $product->subcategory->category->family_id;
+    }
+
+    public function boot(){
+        $this->withValidator(function($validator){
+            if($validator->fails()){
+                $this->dispatch('swal', [
+                    'icon' => 'error',
+                    'title' => 'Error!',
+                    'text' => 'Por favor, corrige los errores en el formulario.'
+                ]);
+            }
+        });
+    }
+
+    public function updatedFamilyId($value){
+        $this->category_id = '';
+        $this->productEdit['subcategory_id'] = '';
+    }
+
+    public function updatedCategoryId($value){
+        $this->productEdit['subcategory_id'] = '';
+    }
+
+    #Propiedad computada para traer las categorias de la familia seleccionada
+    #[Computed()]
+    public function categories(){
+        return Category::where('family_id', $this->family_id)->get();
+    }
+
+    #Propiedad computada para traer las subcategorias de la categoria seleccionada
+    #[Computed()]
+    public function subcategories(){
+        return Subcategory::where('category_id', $this->category_id)->get();
+    }
+
+    public function store(){
+
+        $this->validate([
+            'image' => 'nullable|image|max:1024',
+            'productEdit.sku' => 'required|unique:products,sku,' . $this->product->id,
+            'productEdit.name' => 'required|max:255',
+            'productEdit.description' => 'nullable',
+            'productEdit.price' => 'required|numeric|min:0',
+            'productEdit.subcategory_id' => 'required|exists:subcategories,id',
+        ]);
+
+        if($this->image){
+
+            Storage::delete($this->productEdit['image_path']);
+
+            $this->productEdit['image_path'] = $this->image->store('products');
+        }
+
+        $this->product->update($this->productEdit);
+
+        session()->flash('swal', [
+            'icon' => 'success',
+            'title' => 'Bien hecho!',
+            'text' => 'Producto actualizado correctamente.'
+        ]);
+
+        return redirect()->route('admin.products.edit', $this->product);
+
     }
 
     public function render()
--- resources/views/livewire/admin/products/product-edit.blade.php
+++ resources/views/livewire/admin/products/product-edit.blade.php
@@ -1,3 +1,129 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <form wire:submit="store">
+        <figure class="mb-4 relative">
+
+            <div class="absolute top-8 right-8">
+                <label class="flex items-center px-4 py-2 rounded-lg bg-white cursor-pointer text-gray-700">
+                    <i class="fas fa-camera mr-2"></i>
+                    Actualizar Imagen
+                    <input type="file" class="hidden" accept="image/*" wire:model="image">
+                </label>
+            </div>
+
+            <img class="aspect-[4/3] object-cover object-center w-full" src="{{ $image ? $image->temporaryUrl() : Storage::url($productEdit['image_path']) }}" alt="">
+        </figure>
+
+        <x-validation-errors class="mb-4" />
+
+        <div class="card">
+            
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Código
+                </label>
+
+                <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                    wire:model="productEdit.sku"
+                    placeholder="Ingrese el código del producto"
+                    />
+            </div>
+
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Nombre
+                </label>
+
+                <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                    wire:model="productEdit.name"
+                    placeholder="Ingrese el nombre del producto"
+                    />
+            </div>
+            
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Descripción
+                </label>
+
+                <textarea class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                    wire:model="productEdit.description"
+                    placeholder="Ingrese la descripción del producto">
+                </textarea>
+            </div>
+
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Familias
+                </label>
+
+                <x-select class="w-full" wire:model.live="family_id">
+
+                    <option value="" disabled>
+                        Seleccione una familia
+                    </option>
+
+                    @foreach ($families as $family)
+                        <option value="{{ $family->id }}">
+                            {{ $family->name }}
+                        </option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Categorías
+                </label>
+
+                <x-select class="w-full" wire:model.live="category_id">
+
+                    <option value="" disabled>
+                        Seleccione una categoría
+                    </option>
+
+                    @foreach ($this->categories as $category)
+                        <option value="{{ $category->id }}">
+                            {{ $category->name }}
+                        </option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Subcategorías
+                </label>
+
+                <x-select class="w-full" wire:model.live="productEdit.subcategory_id">
+
+                    <option value="" disabled>
+                        Seleccione una subcategoría
+                    </option>
+
+                    @foreach ($this->subcategories as $subcategory)
+                        <option value="{{ $subcategory->id }}">
+                            {{ $subcategory->name }}
+                        </option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <div class="mb-4">
+                <label for="" class="block mb-2.5 text-sm font-medium text-heading">
+                    Precio
+                </label>
+
+                <input type="number" step="0.01" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                    wire:model="productEdit.price"
+                    placeholder="Ingrese el precio del producto"
+                    />
+            </div>
+
+            <div class="flex justify-end">
+                <button type="submit" class="btn btn-green">
+                    Actualizar Producto
+                </button>
+            </div>
+
+        </div>
+    </form>
 </div>
